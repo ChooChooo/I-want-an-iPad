34931a0d702343ff32a74344bebf2baf0b790a84
ChooChooo <alex.hristov@careerbuilder.com>
2014-05-28 15:09:34 -0400

    removing comments

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,31 +3,23 @@ require 'skill'
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
-  # GET /projects
-  # GET /projects.json
   def index
     @projects = Project.all
   end
 
-  # GET /projects/1
-  # GET /projects/1.json
   def show
     @skill_list = parse_skills
   end
 
-  # GET /projects/new
   def new
     get_skills
     @project = Project.new
   end
 
-  # GET /projects/1/edit
   def edit
     get_skills
   end
 
-  # POST /projects
-  # POST /projects.json
   def create
     @project = Project.new(project_params)
 
@@ -42,8 +34,6 @@ class ProjectsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /projects/1
-  # PATCH/PUT /projects/1.json
   def update
     respond_to do |format|
       if @project.update(project_params)
@@ -56,8 +46,6 @@ class ProjectsController < ApplicationController
     end
   end
 
-  # DELETE /projects/1
-  # DELETE /projects/1.json
   def destroy
     @project.destroy
     respond_to do |format|
@@ -67,7 +55,6 @@ class ProjectsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_project
       @project = Project.find(params[:id])
     end
