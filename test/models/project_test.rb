179fd96ac12220fb8b2deacccad23d0a70310f04
James Tharpe <jamestharpe@careerbuilder.com>
2014-06-19 14:56:25 -0400

    Adds validation to project.name.

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
+require 'project'
 
 class ProjectTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'project attributes must not be empt' do
+    project = Project.new
+    assert project.invalid?
+  end
 end
