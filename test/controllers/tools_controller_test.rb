33c35af8d88aba59b64ca4ac68daa559a1498e40
James Tharpe <jamestharpe@careerbuilder.com>
2014-06-19 16:09:51 -0400

    Checks tools index for listing tools

--- test/controllers/tools_controller_test.rb
+++ test/controllers/tools_controller_test.rb
@@ -9,6 +9,7 @@ class ToolsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:tools)
+    assert_select '.tool_item', Tool.count
   end
 
   test "should get new" do
