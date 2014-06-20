79aaa792a31e65e0cf014bc31c91fca46ed38811
James Tharpe <jamestharpe@careerbuilder.com>
2014-06-20 03:47:01 -0400

    Adds project type support to search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,7 @@ class HomeController < ApplicationController
   def hello
     unless params["search_params"].blank?
       @search_term = params["search_params"]
-
-      @results = Project.where("description like ? or name like ?", "%#{@search_term}%", "%#{@search_term}%")
+      @results = Project.joins(:project_type).where("projects.description like ? or projects.name like ? or project_types.name like ?", "%#{@search_term}%", "%#{@search_term}%", "%#{@search_term}%")
     end
   end
   
