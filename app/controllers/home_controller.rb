7b5607b18fcc546a41f42994cdafeb43898fd6ca
James Tharpe <jamestharpe@careerbuilder.com>
2014-06-28 14:10:48 -0400

    Improves fixture data
    Now easier to test tool/project relationship.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     unless params["search_params"].blank?
       @search_term = params["search_params"]
       @results = Project.joins(:project_type).joins(:tools).where(
-          "projects.description like ? or projects.name like ? or project_types.name like ? or tools.name like ?",
+          'projects.description like ? or projects.name like ? or project_types.name like ? or tools.name like ?',
           "%#{@search_term}%", "%#{@search_term}%", "%#{@search_term}%", "%#{@search_term}%").distinct
     end
   end
--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -16,7 +16,7 @@ class ProjectsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should create project" do
+  test 'should create project' do
     assert_difference('Project.count') do
       post :create,
            project: {
@@ -24,13 +24,13 @@ class ProjectsControllerTest < ActionController::TestCase
                name: @project.name,
                owner: @project.owner,
                project_type_id: @project.project_type_id,
-               projects_tools_attributes: [0, [{tool_id: tools(:one).id }]]}
+               projects_tools_attributes: [0, [{tool_id: tools(:without_projects).id }]]}
     end
 
     assert_redirected_to project_path(assigns(:project))
   end
 
-  test "should show project" do
+  test 'should show project' do
     get :show, id: @project
     assert_response :success
     assert_select 'h1', @project.name
@@ -38,7 +38,7 @@ class ProjectsControllerTest < ActionController::TestCase
     assert_select '.detail_description', @project.description
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get :edit, id: @project
     assert_response :success
   end
@@ -49,11 +49,11 @@ class ProjectsControllerTest < ActionController::TestCase
               description: @project.description,
               name: @project.name,
               owner: @project.owner,
-              projects_tools_attributes: [0, [{tool_id: tools(:one).id }]] }
+              projects_tools_attributes: [0, [{tool_id: tools(:without_projects).id }]] }
     assert_redirected_to project_path(assigns(:project))
   end
 
-  test "should destroy project" do
+  test 'should destroy project' do
     assert_difference('Project.count', -1) do
       delete :destroy, id: @project
     end
