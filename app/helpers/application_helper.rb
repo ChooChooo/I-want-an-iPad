055dc9ff1c1fa6506921ed4247d8b14fd6995c0e
Christina Chatham <christina.chatham@careerbuilder.com>
2014-09-18 22:02:33 -0400

    Fixing a new theme's styles.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,3 @@
 module ApplicationHelper
-  def link_projects_tools_add(name, f)
-    projects_tool = f.object.class.reflect_on_association(:projects_tools).klass.new
-    fields = f.fields_for(:projects_tools, projects_tool, :child_idex => "new_#{:projects_tools}") do |builder|
-      render('/shared/_projects_tools_fields.html.erb', :f => builder)
-    end
-    link_to_function(name, h("add_fields(this, \"#{:projects_tools}\", \"#{escape_javascript(fields)}\")"))
-  end
+
 end
