1ea7987b813340a93a32604b9eb768a517deddf0
JHambrick <Jarvis.Hambrick@careerbuilder.com>
2014-09-19 01:27:43 -0400

    restyling and adding project_type to attribute

--- app/models/project.rb
+++ app/models/project.rb
@@ -9,7 +9,7 @@ class Project < ActiveRecord::Base
   accepts_nested_attributes_for :projects_tools, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
   accepts_nested_attributes_for :tools, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
 
-  attr_accessible :name, :description, :projects_tools, :project_type_id, :owner
+  attr_accessible :name, :description, :projects_tools, :project_type_id, :owner, :project_type
   validates :name,
             :project_type_id,
             #:owner,
