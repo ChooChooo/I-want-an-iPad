a76aa2df82c220d08cbae7240e8bcf0bebdce871
ChooChooo <alex.hristov@careerbuilder.com>
2014-12-10 20:17:24 -0500

    gfhgfh

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,17 +3,18 @@ require 'octokit'
 class HomeController < ApplicationController
   
   def hello
-    client = Octokit::Client.new(:client_id     => "696d5200fd62d6d03d14",
-                                 :client_secret => "09142a11d387ec87d024d902cbbb0b0785374040")
+    client = Octokit::Client.new(:client_id     => "6969eaab25c99d040903",
+                                 :client_secret => "69868a6661379b0f1802d99f78ee95e7953c6116")
                                  
                                  #(:client_id     => "6969eaab25c99d040903",
                                  #:client_secret => "69868a6661379b0f1802d99f78ee95e7953c6116")
                                  #(:client_id     => "696d5200fd62d6d03d14",
                                  #:client_secret => "09142a11d387ec87d024d902cbbb0b0785374040")
   
-    #@user = Octokit.user 'ChooChooo'
-  
-    @user = client.user 'ChooChooo'
+    #owner 'ChooChooo'
+    #repo 'I-want-an-iPad'
+    
+    @output = client.user 'btidwell'
     
     unless params["search_params"].blank?
       @search_term = params["search_params"]
