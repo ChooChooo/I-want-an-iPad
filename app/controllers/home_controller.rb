05375616c094e71a4d8c9032a97f423c429d0dbd
Christina Chatham <christina.chatham@careerbuilder.com>
2014-12-11 11:28:36 -0500

    Search has a page now.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,22 +15,6 @@ class HomeController < ApplicationController
     #repo 'I-want-an-iPad'
     
     @output = client.user 'btidwell'
-    
-    unless params["search_params"].blank?
-      @search_term = params["search_params"]
-      results = PgSearch.multisearch(@search_term)
-      
-      @results_tools = Hash.new
-      @results_projects = Hash.new
-
-      results.each do |result|
-        if result.searchable_type == "Tool"
-          @results_tools[result] = Tool.find(result.searchable_id, :select => :name)
-        else
-          @results_projects[result] = Project.find(result.searchable_id, :select => :name)
-        end  
-      end
-    end
 
   end
   
