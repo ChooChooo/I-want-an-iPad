e24dba7343241636c2572a0a62657067e0482de0
James Tharpe <jamestharpe@careerbuilder.com>
2014-12-11 13:18:19 -0500

    Fixes Alex's crappy search code... sort of

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,17 +1,17 @@
 class SearchController < ApplicationController
 
   def search
-    unless params["search_params"].blank?
-      @search_term = params["search_params"]
+    unless params['search_params'].blank?
+      @search_term = params['search_params']
       results = PgSearch.multisearch(@search_term)
 
       @results_tools = Hash.new
       @results_projects = Hash.new
 
       results.each do |result|
-        if result.searchable_type == "Tool"
+        if result.searchable_type == 'Tool'
           @results_tools[result] = Tool.find(result.searchable_id, :select => :name)
-        else
+        elsif result.searchable_type == 'Project'
           @results_projects[result] = Project.find(result.searchable_id, :select => :name)
         end
       end
