bcf041c61d0af5fb555f669fefa2fb913770e1e6
btidwell <ben.tidwell@careerbuilder.com>
2014-12-12 00:39:35 -0500

    GRAVATAR!!!

--- app/controllers/user_view_controller.rb
+++ app/controllers/user_view_controller.rb
@@ -2,7 +2,12 @@ class UserViewController < ApplicationController
   before_action :set_user_and_projects, only: [:show]
 
   def show
+    require 'digest/md5'
+    # create the md5 hash
+    hash = Digest::MD5.hexdigest(@user.email)
 
+    # compile URL which can be used in <img src="RIGHT_HERE"...
+    @gravatar_src = "http://www.gravatar.com/avatar/#{hash}"
   end
 
 
